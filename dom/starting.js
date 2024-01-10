e7b6a8836d34ccc47e51952758a2d5e43313b26c
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2024-01-10 17:24:47 -0300

    starting dom

---
+++ dom/starting.js
@@ -0,0 +1,22 @@
+//DOM (Document Object Model)
+// Selecting
+
+// getElementById()
+
+//Elements
+// getElementsById()
+// getElementsByTagName
+// getElementsByClassName
+
+
+// querySelector
+// Give us the first match from the search
+document.querySelector('a')
+
+// querySelectorAll
+
+
+//changing style
+const spans = document.querySelector('span');
+spans.style.color = 'red'
+
